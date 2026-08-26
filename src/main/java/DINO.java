04b5f95b5a2129f04bd66a45d3c9a667a93f4f0a
WillChin484848 <willchin2004@gmail.com>
2026-08-26 12:43:30 +0800

    Level 0

--- src/main/java/DINO.java
+++ src/main/java/DINO.java
@@ -1,5 +1,12 @@
 public class DINO {
     public static void main(String[] args) {
+        System.out.println(" ____    ___   _   _    ___  ");
+        System.out.println("|  _ \\  |_ _| | \\ | |  / _ \\ ");
+        System.out.println("| | | |  | |  |  \\| | | | | |");
+        System.out.println("| |_| |  | |  | |\\  | | |_| |");
+        System.out.println("|____/  |___| |_| \\_|  \\___/ ");
+
+        System.out.println();
         System.out.println("Hello! I'm DINO");
         System.out.println("What can I do for you?");
         System.out.println();
