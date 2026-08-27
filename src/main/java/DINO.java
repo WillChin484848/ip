7db3e82b682d4146bb802d7a8ccc1de24f699829
WillChin484848 <willchin2004@gmail.com>
2026-08-27 22:08:36 +0800

    Level 1

--- src/main/java/DINO.java
+++ src/main/java/DINO.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class DINO {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(" ____    ___   _   _    ___  ");
         System.out.println("|  _ \\  |_ _| | \\ | |  / _ \\ ");
         System.out.println("| | | |  | |  |  \\| | | | | |");
@@ -10,6 +14,18 @@ public class DINO {
         System.out.println("Hello! I'm DINO");
         System.out.println("What can I do for you?");
         System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
