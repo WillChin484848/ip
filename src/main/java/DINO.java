511c319768059595251e34e6d1684110c12b026c
WillChin484848 <willchin2004@gmail.com>
2026-08-27 22:18:39 +0800

    level 3 part 2

--- src/main/java/DINO.java
+++ src/main/java/DINO.java
@@ -4,8 +4,7 @@ public class DINO {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
-        boolean[] isDone = new boolean[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println(" ____    ___   _   _    ___  ");
@@ -30,25 +29,31 @@ public class DINO {
                 System.out.println("Here are the tasks in your list:");
 
                 for (int i = 0; i < taskCount; i++) {
-                    String status = isDone[i] ? "X" : " ";
-                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
 
             } else if (input.startsWith("mark ")) {
                 int taskNumber = Integer.parseInt(input.substring(5));
                 int index = taskNumber - 1;
 
-                isDone[index] = true;
+                tasks[index].markAsDone();
 
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[X] " + tasks[index]);
+                System.out.println(tasks[index]);
+
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                int index = taskNumber - 1;
+
+                tasks[index].markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
 
             } else {
-                tasks[taskCount] = input;
-                isDone[taskCount] = false;
+                tasks[taskCount] = new Task(input);
+                System.out.println("added: " + tasks[taskCount]);
                 taskCount++;
-
-                System.out.println("added: " + input);
             }
         }
 
