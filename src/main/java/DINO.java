201e213303746da5f464b7eea2ff5900fa2c72dd
WillChin484848 <willchin2004@gmail.com>
2026-08-27 22:13:42 +0800

    Add mark command

--- src/main/java/DINO.java
+++ src/main/java/DINO.java
@@ -5,6 +5,7 @@ public class DINO {
         Scanner scanner = new Scanner(System.in);
 
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         System.out.println(" ____    ___   _   _    ___  ");
@@ -24,14 +25,27 @@ public class DINO {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+
             } else if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
 
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println((i + 1) + ".[" + status + "] " + tasks[i]);
                 }
+
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                int index = taskNumber - 1;
+
+                isDone[index] = true;
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[index]);
+
             } else {
                 tasks[taskCount] = input;
+                isDone[taskCount] = false;
                 taskCount++;
 
                 System.out.println("added: " + input);
